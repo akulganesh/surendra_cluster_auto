18bf1386c85b7d095b31d47894af698115385cc8
akulganesh <46016314+akulganesh@users.noreply.github.com>
2019-02-18 10:40:47 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,11 @@
 wget https://dev.mysql.com/get/archives/mysql-5.7/mysql-5.7.19-1.el7.x86_64.rpm-bundle.tar
 tar -xf mysql-5.7.19-1.el7.x86_64.rpm-bundle.tar
 yum remove mariadb* -y
-yum install -y /root/mysql-community-common-5.7.19-1.el7.x86_64.rpm
-yum install -y /root/mysql-community-libs-5.7.19-1.el7.x86_64.rpm
-yum install -y /root/mysql-community-libs-compat-5.7.19-1.el7.x86_64.rpm
-yum install -y /root/mysql-community-devel-5.7.19-1.el7.x86_64.rpm
-yum install -y /root/mysql-community-client-5.7.19-1.el7.x86_64.rpm
-yum install -y /root/mysql-community-server-5.7.19-1.el7.x86_64.rpm
+yum install -y mysql-community-common-5.7.19-1.el7.x86_64.rpm
+yum install -y mysql-community-libs-5.7.19-1.el7.x86_64.rpm
+yum install -y mysql-community-libs-compat-5.7.19-1.el7.x86_64.rpm
+yum install -y mysql-community-devel-5.7.19-1.el7.x86_64.rpm
+yum install -y mysql-community-client-5.7.19-1.el7.x86_64.rpm
+yum install -y mysql-community-server-5.7.19-1.el7.x86_64.rpm
 service mysqld start
 cat /var/log/mysql.log | grep temp
